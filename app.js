5b234d40ee3536c5fc779ed5130caf1699bd1260
Muhammad S. Maromi <sohibul.maromi@mediatrac.co.id>
2017-11-26 13:10:50 +0700

    Display data using mapbox

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const redis = require('redis');
 const express = require('express');
+const server = require('server');
 
 const app = express();
 
@@ -10,19 +11,54 @@ const { recreateData } = require('./helper');
 const WPI_API = require('./config').WPI;
 // database client
 const dbClient = redis.createClient();
+// 
+// request(WPI_API, async (error, response, body) => {
+//   const bodyObj = JSON.parse(body);
+//   const dataList = bodyObj.data;
 
-request(WPI_API, async (error, response, body) => {
-  const bodyObj = JSON.parse(body);
-  const dataList = bodyObj.data;
+//   const timestamp = new Date().getTime();
+//   let createdDataList = [];
+//   // iterate over location
+//   for (const data of dataList) {
+//     const createdData = await recreateData(data);
+//     createdDataList.push(createdData);
+//   }
+  
+//   dbClient.zadd(['data', timestamp, JSON.stringify(createdDataList)], redis.print);
+// });
 
-  const timestamp = new Date().getTime();
-  let createdDataList = [];
-  // iterate over location
-  for (const data of dataList) {
-    const createdData = await recreateData(data);
-    createdDataList.push(createdData);
+// --------------------
+// --- API server ---//
+// --------------------
+const { get, post } = server.router;
+const { render, json, header } = server.reply;
+
+const getData = get('/data', async ctx => {
+  let p = new Promise((resolve, reject) => {
+    // get last data
+    dbClient.zrange('data', -1, -1, (error, reply) => {
+      if(error) reject(error)
+      resolve(reply)
+    })
+  })
+
+  let data = null
+  try {
+    data = await p;
+  } catch (error) {
+    console.log(error)
   }
   
-  dbClient.zadd(['data', timestamp, JSON.stringify(createdDataList)], redis.print);
+  return JSON.parse(data[0]);
 });
+
+server(
+  { port: 3333 }, 
+  [
+    ctx => header("Access-Control-Allow-Origin", "*"),
+    ctx => header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),    
+    getData
+  ]
+);
+
 
