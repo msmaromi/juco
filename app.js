4397d181c8b446629caa8d53f47923838c5ff3ba
Muhammad S. Maromi <sohibul.maromi@mediatrac.co.id>
2017-12-21 17:22:25 +0700

    Using dummy data

--- app.js
+++ app.js
@@ -3,22 +3,48 @@ const redis = require('redis');
 const express = require('express');
 const server = require('server');
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const app = express();
+const dummyData = require('./dummy');
 
 const { recreateData } = require('./helper');
 
 // API from warta pasar ikan website
 const WPI_API = require('./config').WPI;
 // database client
-const dbClient = redis.createClient();
-// 
+// const dbClient = redis.createClient();
+mongoose.connect('mongodb://localhost/juco', {
+  useMongoClient: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+const DataSchema = new Schema({
+  data: Schema.Types.Mixed
+});
+
+const Data = mongoose.model('Data', DataSchema);
+let data = new Data({ data: dummyData });
+
+db.once('open', function() {
+  // data.save((err, data) => {
+  //   if (err) return console.error(err);
+    
+  //   console.log("Data saved", data);
+  // })
+});
+
+// !!! wpi.kkp.go.id cannot be accessed !!!
 // request(WPI_API, async (error, response, body) => {
+//   console.log(body);
 //   const bodyObj = JSON.parse(body);
 //   const dataList = bodyObj.data;
 
 //   const timestamp = new Date().getTime();
 //   let createdDataList = [];
-//   // iterate over location
+//   iterate over location
 //   for (const data of dataList) {
 //     const createdData = await recreateData(data);
 //     createdDataList.push(createdData);
@@ -27,38 +53,18 @@ const dbClient = redis.createClient();
 //   dbClient.zadd(['data', timestamp, JSON.stringify(createdDataList)], redis.print);
 // });
 
+
 // --------------------
 // --- API server ---//
 // --------------------
 const { get, post } = server.router;
 const { render, json, header } = server.reply;
 
-const getData = get('/data', async ctx => {
-  let p = new Promise((resolve, reject) => {
-    // get last data
-    dbClient.zrange('data', -1, -1, (error, reply) => {
-      if(error) reject(error)
-      resolve(reply)
-    })
-  })
-
-  let data = null
-  try {
-    data = await p;
-  } catch (error) {
-    console.log(error)
-  }
-  
-  return JSON.parse(data[0]);
-});
-
 server(
   { port: 3333 }, 
   [
     ctx => header("Access-Control-Allow-Origin", "*"),
     ctx => header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),    
-    getData
+    get('/data', ctx => dummyData)
   ]
 );
-
-
---
+++ dummy.js
@@ -0,0 +1,1000 @@
+module.exports = [{
+  "city": "Kab.Banyuasin",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "21,000"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "45,000"
+  }, {
+    "type": "kembung",
+    "price": "26,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "25,000"
+  }, {
+    "type": "nila",
+    "price": "25,000"
+  }, {
+    "type": "patin",
+    "price": "21,000"
+  }, {
+    "type": "tongkol",
+    "price": "25,000"
+  }, {
+    "type": "udang putih",
+    "price": "60,000"
+  }],
+  "coordinate": {
+    "lat": -2.6095639,
+    "lng": 104.7520939
+  }
+}, {
+  "city": "Kab.Berau",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "24,000"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "35,000"
+  }, {
+    "type": "layang",
+    "price": "30,000"
+  }, {
+    "type": "lele",
+    "price": "30,000"
+  }, {
+    "type": "nila",
+    "price": "40,000"
+  }, {
+    "type": "patin",
+    "price": "45,000"
+  }, {
+    "type": "tongkol",
+    "price": "25,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": 2.0450883,
+    "lng": 117.3616476
+  }
+}, {
+  "city": "Kab.Jembrana",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "40,000"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "25,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -8.3233438,
+    "lng": 114.6667939
+  }
+}, {
+  "city": "Kab.Kubu Raya",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "21,291"
+  }, {
+    "type": "cakalang",
+    "price": "23,680"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "25,397"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "25,000"
+  }, {
+    "type": "nila",
+    "price": "27,646"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "23,588"
+  }, {
+    "type": "udang putih",
+    "price": "90,000"
+  }],
+  "coordinate": {
+    "lat": -0.3533938,
+    "lng": 109.4735066
+  }
+}, {
+  "city": "Kab.Limapuluhkota",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "40,000"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "20,000"
+  }, {
+    "type": "nila",
+    "price": "35,000"
+  }, {
+    "type": "patin",
+    "price": "25,000"
+  }, {
+    "type": "tongkol",
+    "price": "23,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": 0.07341919999999999,
+    "lng": 100.5296115
+  }
+}, {
+  "city": "Kab.Lombok Barat",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "28,000"
+  }, {
+    "type": "cakalang",
+    "price": "45,000"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "38,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "24,000"
+  }, {
+    "type": "nila",
+    "price": "30,000"
+  }, {
+    "type": "patin",
+    "price": "28,000"
+  }, {
+    "type": "tongkol",
+    "price": "25,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -8.6464599,
+    "lng": 116.1123078
+  }
+}, {
+  "city": "Kab.Majalengka",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "28,000"
+  }, {
+    "type": "cakalang",
+    "price": "30,000"
+  }, {
+    "type": "gurami",
+    "price": "40,000"
+  }, {
+    "type": "kembung",
+    "price": "30,000"
+  }, {
+    "type": "layang",
+    "price": "30,000"
+  }, {
+    "type": "lele",
+    "price": "21,000"
+  }, {
+    "type": "nila",
+    "price": "25,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "-"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -6.7790605,
+    "lng": 108.2852049
+  }
+}, {
+  "city": "Kab.Muna",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "30,000"
+  }, {
+    "type": "cakalang",
+    "price": "26,000"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "26,698"
+  }, {
+    "type": "layang",
+    "price": "25,000"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "26,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -4.901629,
+    "lng": 122.6277455
+  }
+}, {
+  "city": "Kab.Musi Banyuasin",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "48,000"
+  }, {
+    "type": "kembung",
+    "price": "34,000"
+  }, {
+    "type": "layang",
+    "price": "20,000"
+  }, {
+    "type": "lele",
+    "price": "23,000"
+  }, {
+    "type": "nila",
+    "price": "32,000"
+  }, {
+    "type": "patin",
+    "price": "21,000"
+  }, {
+    "type": "tongkol",
+    "price": "27,000"
+  }, {
+    "type": "udang putih",
+    "price": "105,000"
+  }],
+  "coordinate": {
+    "lat": -2.5442029,
+    "lng": 103.7289167
+  }
+}, {
+  "city": "Kab.Musi Rawas",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "46,000"
+  }, {
+    "type": "kembung",
+    "price": "30,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "23,000"
+  }, {
+    "type": "nila",
+    "price": "31,000"
+  }, {
+    "type": "patin",
+    "price": "20,000"
+  }, {
+    "type": "tongkol",
+    "price": "33,000"
+  }, {
+    "type": "udang putih",
+    "price": "105,000"
+  }],
+  "coordinate": {
+    "lat": -3.0956537,
+    "lng": 103.0817903
+  }
+}, {
+  "city": "Kab.Nganjuk",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "34,357"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -7.594350700000001,
+    "lng": 111.9045541
+  }
+}, {
+  "city": "Kab.Ogan Ilir",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "22,000"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "40,000"
+  }, {
+    "type": "kembung",
+    "price": "26,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "22,000"
+  }, {
+    "type": "nila",
+    "price": "28,000"
+  }, {
+    "type": "patin",
+    "price": "22,000"
+  }, {
+    "type": "tongkol",
+    "price": "24,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -3.426544,
+    "lng": 104.6121475
+  }
+}, {
+  "city": "Kab.Ogan Komering Ulu",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "20,000"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "42,000"
+  }, {
+    "type": "kembung",
+    "price": "30,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "23,000"
+  }, {
+    "type": "nila",
+    "price": "35,000"
+  }, {
+    "type": "patin",
+    "price": "23,000"
+  }, {
+    "type": "tongkol",
+    "price": "20,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -4.0283486,
+    "lng": 104.0072348
+  }
+}, {
+  "city": "Kab.Pangandaran",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "36,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "39,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -7.615061099999999,
+    "lng": 108.4988269
+  }
+}, {
+  "city": "Kab.Pasaman",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "25,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "32,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": 0.2209392,
+    "lng": 100.1703257
+  }
+}, {
+  "city": "Kab.Penajam Paser Utara",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "20,917"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "35,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "40,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "25,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -1.2917094,
+    "lng": 116.5137964
+  }
+}, {
+  "city": "Kab.Pesisir Selatan",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "21,235"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "26,778"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "30,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "30,939"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -1.7223147,
+    "lng": 100.8903099
+  }
+}, {
+  "city": "Kab.Seram Bagian Barat",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "30,000"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "20,000"
+  }, {
+    "type": "layang",
+    "price": "20,000"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "-"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -3.1271575,
+    "lng": 128.4008357
+  }
+}, {
+  "city": "Kab.Situbondo",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "23,600"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "-"
+  }, {
+    "type": "udang putih",
+    "price": "52,000"
+  }],
+  "coordinate": {
+    "lat": -7.7888522,
+    "lng": 114.1914951
+  }
+}, {
+  "city": "Kab.Timor Tengah Utara",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "60,000"
+  }, {
+    "type": "nila",
+    "price": "60,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "-"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -9.452264699999999,
+    "lng": 124.597132
+  }
+}, {
+  "city": "Kota Lubuk Linggau",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "24,000"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "50,000"
+  }, {
+    "type": "kembung",
+    "price": "30,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "22,000"
+  }, {
+    "type": "nila",
+    "price": "27,500"
+  }, {
+    "type": "patin",
+    "price": "22,000"
+  }, {
+    "type": "tongkol",
+    "price": "22,000"
+  }, {
+    "type": "udang putih",
+    "price": "120,000"
+  }],
+  "coordinate": {
+    "lat": -3.2995858,
+    "lng": 102.857236
+  }
+}, {
+  "city": "Kota Malang",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "-"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "21,200"
+  }, {
+    "type": "nila",
+    "price": "28,429"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "-"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -7.9666204,
+    "lng": 112.6326321
+  }
+}, {
+  "city": "Kota Pagar Alam",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "45,000"
+  }, {
+    "type": "kembung",
+    "price": "22,000"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "27,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "22,000"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -4.0419617,
+    "lng": 103.2278845
+  }
+}, {
+  "city": "Kota Palembang",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "22,000"
+  }, {
+    "type": "cakalang",
+    "price": "24,000"
+  }, {
+    "type": "gurami",
+    "price": "45,000"
+  }, {
+    "type": "kembung",
+    "price": "30,000"
+  }, {
+    "type": "layang",
+    "price": "22,000"
+  }, {
+    "type": "lele",
+    "price": "21,000"
+  }, {
+    "type": "nila",
+    "price": "15,000"
+  }, {
+    "type": "patin",
+    "price": "17,500"
+  }, {
+    "type": "tongkol",
+    "price": "22,000"
+  }, {
+    "type": "udang putih",
+    "price": "80,000"
+  }],
+  "coordinate": {
+    "lat": -2.9760735,
+    "lng": 104.7754307
+  }
+}, {
+  "city": "Kota Pangkal Pinang",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "35,667"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "24,621"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "28,000"
+  }, {
+    "type": "tongkol",
+    "price": "27,611"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -2.1316266,
+    "lng": 106.1169299
+  }
+}, {
+  "city": "Kota Probolinggo",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "19,000"
+  }, {
+    "type": "cakalang",
+    "price": "-"
+  }, {
+    "type": "gurami",
+    "price": "35,000"
+  }, {
+    "type": "kembung",
+    "price": "20,000"
+  }, {
+    "type": "layang",
+    "price": "16,000"
+  }, {
+    "type": "lele",
+    "price": "20,000"
+  }, {
+    "type": "nila",
+    "price": "25,000"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "20,000"
+  }, {
+    "type": "udang putih",
+    "price": "50,000"
+  }],
+  "coordinate": {
+    "lat": -7.776422600000001,
+    "lng": 113.2037131
+  }
+}, {
+  "city": "Kota Sorong",
+  "fishes": [{
+    "type": "bandeng",
+    "price": "-"
+  }, {
+    "type": "cakalang",
+    "price": "33,692"
+  }, {
+    "type": "gurami",
+    "price": "-"
+  }, {
+    "type": "kembung",
+    "price": "24,780"
+  }, {
+    "type": "layang",
+    "price": "-"
+  }, {
+    "type": "lele",
+    "price": "-"
+  }, {
+    "type": "nila",
+    "price": "-"
+  }, {
+    "type": "patin",
+    "price": "-"
+  }, {
+    "type": "tongkol",
+    "price": "-"
+  }, {
+    "type": "udang putih",
+    "price": "-"
+  }],
+  "coordinate": {
+    "lat": -0.8761629000000001,
+    "lng": 131.255828
+  }
+}]
